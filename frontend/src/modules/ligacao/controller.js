40350dbdf43522b2e2391f607c8aac6b95ad2c08
Eduardo Martins <edusnets@gmail.com>
2017-08-21 15:41:18 -0300

    Corrigido exibição do tempo das ligações

--- frontend/src/modules/ligacao/controller.js
+++ frontend/src/modules/ligacao/controller.js
@@ -32,6 +32,25 @@
              
             $scope.rows = [];
 
+            function fancyTimeFormat(time)
+            {   
+                // Hours, minutes and seconds
+                var hrs = ~~(time / 3600);
+                var mins = ~~((time % 3600) / 60);
+                var secs = time % 60;
+
+                // Output like "1:01" or "4:03:59" or "123:03:59"
+                var ret = "";
+
+                if (hrs > 0) {
+                    ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+                }
+
+                ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+                ret += "" + secs;
+                return ret;
+            }
+
             $scope.fields = [{
                 key: 'date',
                 title: 'Data',
@@ -53,12 +72,18 @@
                 key: 'duracao',
                 title: 'Duração',
                 sortable: true,
-                hide: 'hidden-sm hidden-xs'
+                hide: 'hidden-sm hidden-xs',
+                onRender: function(val){
+                  return fancyTimeFormat(val);
+                }
             }, {
                 key: 'faturado',
                 title: 'Tarifado',
                 sortable: true,
-                hide: 'hidden-sm hidden-xs'
+                hide: 'hidden-sm hidden-xs',
+                onRender: function(val){
+                  return fancyTimeFormat(val);
+                }
             },{
                 key: 'status',
                 title: 'Status',
