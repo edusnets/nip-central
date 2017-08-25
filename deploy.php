a7c6cac070dc2abb044536db2bd390c8bac601ea
Saul Luz <saul.luz@nextip.com.br>
2017-08-25 19:46:42 -0300

    Removed file deploy

--- deploy.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-exec("sshpass -p 'root' scp -rp /home/saul/Projects/nip-central/frontend/dist/* root@192.168.63.95:/var/www/frontend");
-exec("sshpass -p 'root' scp -rp /home/saul/Projects/nip-central/backend/* root@192.168.63.95:/var/www/backend");
-
-?>
